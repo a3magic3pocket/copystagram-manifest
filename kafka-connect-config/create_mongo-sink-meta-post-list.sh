ea3794b6b15ac95c4a473fdaff1f981fe6cdcc4f
a3magic3pocket <a3magic3pocket@gmail.com>
2024-04-20 05:14:34 +0900

    Create kafka connect config.

---
+++ kafka-connect-config/create_mongo-sink-meta-post-list.sh
@@ -0,0 +1,23 @@
+curl --location 'http://your-server-ip:8083/connectors' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name": "mongo-sink-meta-post-list",
+    "config": {
+        "connector.class": "com.mongodb.kafka.connect.MongoSinkConnector",
+        "connection.uri": "mongodb+srv://your-mongodb-id:your-mongodb-pw@your-mongodb-atlas-domain/",
+        "tasks.max": "1",
+        "topics": "meta-post-list-result",
+        "database": "copystagram",
+        "collection": "metaPostList",
+        "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+        "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "key.converter.schemas.enable": false,
+        "value.converter.schemas.enable": false,
+        "document.id.strategy.overwrite.existing": true,
+        "document.id.strategy": "com.mongodb.kafka.connect.sink.processor.id.strategy.FullKeyStrategy",
+        "writemodel.strategy": "com.mongodb.kafka.connect.sink.writemodel.strategy.ReplaceOneBusinessKeyStrategy",
+        "post.processor.chain": "sinkPostProcessor.ObjectIdPostProcessor",
+        "value.projection.list": "postId"
+    }
+}
+'
